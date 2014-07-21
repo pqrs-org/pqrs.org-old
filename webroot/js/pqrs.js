fa1ee6bec7aa9b17467b6fedfa652689b1540964
Takayama Fumihiko <tekezo@pqrs.org>
2014-07-21 11:58:27 +0000

    add webroot/js

---
+++ webroot/js/pqrs.js
@@ -0,0 +1,39 @@
+!function ($) {
+    /* ------------------------------------------------------------ */
+    /* from bs/assets/js/application.js */
+    var $window = $(window);
+    var $body   = $(document.body);
+
+    var navHeight = $('.navbar').outerHeight(true) + 10;
+
+    $body.scrollspy({
+        target: '.bs-sidebar',
+        offset: navHeight
+    });
+
+    setTimeout(function () {
+        var $sideBar = $('.bs-sidebar');
+
+        $sideBar.affix({
+            offset: {
+                top: function () {
+                    var offsetTop      = $sideBar.offset().top;
+                    var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10);
+                    var navOuterHeight = $('.bs-docs-nav').height();
+
+                    return (this.top = offsetTop - navOuterHeight - sideBarMargin);
+                }
+                , bottom: function () {
+                    return (this.bottom = $('.bs-footer').outerHeight(true));
+                }
+            }
+        });
+    }, 100);
+
+    /* ------------------------------------------------------------ */
+    /* pqrs.org */
+    $('.modal-dialog').click(function () {
+        $(this).parent().modal('hide');
+    });
+
+}(window.jQuery);
