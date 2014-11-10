b774615815314c5c8562ec3715f716c1397170e0
Takayama Fumihiko <tekezo@pqrs.org>
2014-11-10 16:17:37 +0000

    update js for bs-3.3.0

--- webroot/js/pqrs.js
+++ webroot/js/pqrs.js
@@ -1,39 +1,45 @@
 !function ($) {
-    /* ------------------------------------------------------------ */
-    /* from bs/assets/js/application.js */
-    var $window = $(window);
-    var $body   = $(document.body);
+    'use strict';
 
-    var navHeight = $('.navbar').outerHeight(true) + 10;
+    $(function () {
+        /* ------------------------------------------------------------ */
+        /* from bs/assets/js/application.js */
+        var $window = $(window);
+        var $body   = $(document.body);
 
-    $body.scrollspy({
-        target: '.bs-sidebar',
-        offset: navHeight
-    });
+        var navHeight = $('.navbar').outerHeight(true) + 10;
 
-    setTimeout(function () {
-        var $sideBar = $('.bs-sidebar');
-
-        $sideBar.affix({
-            offset: {
-                top: function () {
-                    var offsetTop      = $sideBar.offset().top;
-                    var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10);
-                    var navOuterHeight = $('.bs-docs-nav').height();
-
-                    return (this.top = offsetTop - navOuterHeight - sideBarMargin);
-                }
-                , bottom: function () {
-                    return (this.bottom = $('.bs-footer').outerHeight(true));
-                }
-            }
+        $body.scrollspy({
+            target: '.bs-docs-sidebar'
         });
-    }, 100);
+        $window.on('load', function () {
+            $body.scrollspy('refresh');
+        });
 
-    /* ------------------------------------------------------------ */
-    /* pqrs.org */
-    $('.modal-dialog').click(function () {
-        $(this).parent().modal('hide');
+        setTimeout(function () {
+            var $sideBar = $('.bs-docs-sidebar');
+
+            $sideBar.affix({
+                offset: {
+                    top: function () {
+                        var offsetTop      = $sideBar.offset().top;
+                        var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10);
+                        var navOuterHeight = $('.bs-docs-nav').height();
+
+                        return (this.top = offsetTop - navOuterHeight - sideBarMargin);
+                    }
+                    , bottom: function () {
+                        return (this.bottom = $('.bs-footer').outerHeight(true));
+                    }
+                }
+            });
+        }, 100);
+
+        /* ------------------------------------------------------------ */
+        /* pqrs.org */
+        $('.modal-dialog').click(function () {
+            $(this).parent().modal('hide');
+        });
     });
 
 }(window.jQuery);
