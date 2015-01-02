258ac9898f96c83e36322838306182ebdca751ad
Takayama Fumihiko <tekezo@pqrs.org>
2015-01-02 14:22:57 +0000

    update gallery

--- source/page/osx/karabiner/gallery.rb
+++ source/page/osx/karabiner/gallery.rb
@@ -8,10 +8,24 @@ class Gallery < KarabinerBase
   def tabs_definition
     [
       {
-        :href => '#examples',
+        :href => '#change-either-the-left-or-right-modifier-key',
         :name_l10n => {
-          :en => 'examples',
-          :ja => 'examples',
+          :en => 'Change either the left or right modifier key',
+          :ja => '左右のいずれかのモディファイアを変更する',
+        },
+      },
+      {
+        :href => '#mouse-keys-mode-v2',
+        :name_l10n => {
+          :en => 'Mouse Keys Mode v2',
+          :ja => 'Mouse Keys Mode v2',
+        },
+      },
+      {
+        :href => '#ignore-bouncing',
+        :name_l10n => {
+          :en => 'Ignore bouncing (chattering) events',
+          :ja => 'チャタリングの抑制',
         },
       },
     ]
