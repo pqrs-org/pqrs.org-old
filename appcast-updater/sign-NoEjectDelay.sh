d54353ff722257d4037025604ee28cd8175e73fe
Takayama Fumihiko <tekezo@pqrs.org>
2015-09-23 22:39:18 +0900

    add sign-NoEjectDelay.sh

---
+++ appcast-updater/sign-NoEjectDelay.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+basedir=`dirname $0`
+cd "$basedir"
+
+priv_pem="secret/noejectdelay_priv.pem"
+. scripts/check-priv_pem.sh
+
+targetdir="../webroot/osx/karabiner/files"
+
+latest_dmg=`ruby scripts/get-latest.rb $targetdir/NoEjectDelay-*.dmg`
+version=$(echo `basename $latest_dmg .dmg` | sed 's|NoEjectDelay-||')
+length=`ruby scripts/get-length.rb $latest_dmg`
+dsaSignature=`sh scripts/sign_update.sh $latest_dmg $priv_pem`
+pubDate=`ruby scripts/get-time.rb`
+
+if [ "$version" == `ruby scripts/get-version.rb < "$targetdir/noejectdelay-appcast-devel.xml"` ]; then
+    echo " `basename $0`: Already up-to-date."
+    exit 0
+fi
+
+rm -f "$targetdir/noejectdelay-appcast-devel.xml.tmp"
+
+>>"$targetdir/noejectdelay-appcast-devel.xml.tmp" cat <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:sparkle="http://www.andymatuschak.org/xml-namespaces/sparkle"  xmlns:dc="http://purl.org/dc/elements/1.1/">
+  <channel>
+    <title>NoEjectDelay Changelog</title>
+    <link>https://pqrs.org/osx/karabiner/files/noejectdelay-appcast-devel.xml</link>
+    <description>Most recent changes with links to updates.</description>
+    <language>en</language>
+
+    <item>
+      <title>Version $version</title>
+      <sparkle:minimumSystemVersion>10.9.0</sparkle:minimumSystemVersion>
+      <description><![CDATA[
+
+<!-- RENAME_INFORMATION -->
+
+<h2>About v$version Update</h2>
+<ul>
+  <li>v$version: XXX</li>
+EOF
+
+>>"$targetdir/noejectdelay-appcast-devel.xml.tmp" ruby scripts/get-ul.rb < "$targetdir/noejectdelay-appcast-devel.xml"
+
+>>"$targetdir/noejectdelay-appcast-devel.xml.tmp" cat <<EOF
+</ul>
+<a href="https://pqrs.org/osx/karabiner/noejectdelay.html#history">More</a>
+]]>
+      </description>
+      <pubDate>$pubDate</pubDate>
+      <enclosure url="https://pqrs.org/osx/karabiner/files/NoEjectDelay-$version.dmg"
+                 sparkle:version="$version" length="$length" type="application/octet-stream"
+                 sparkle:dsaSignature="$dsaSignature"
+                 />
+    </item>
+
+  </channel>
+</rss>
+EOF
+
+mv "$targetdir/noejectdelay-appcast-devel.xml.tmp" "$targetdir/noejectdelay-appcast-devel.xml"
+chmod 644 "$targetdir/noejectdelay-appcast-devel.xml"
+echo \
+    '\033[33;40m' \
+    "`basename $0`: noejectdelay-appcast-devel.xml is updated." \
+    '\033[0m'
