c0f30b79f3716a22251c1ce499e6de90d183b4a7
Takayama Fumihiko <tekezo@pqrs.org>
2016-01-10 22:54:06 +0900

    update sign-NoEjectDelay

--- appcast-updater/sign-NoEjectDelay.sh
+++ appcast-updater/sign-NoEjectDelay.sh
@@ -34,17 +34,29 @@ rm -f "$targetdir/noejectdelay-appcast-devel.xml.tmp"
       <title>Version $version</title>
       <sparkle:minimumSystemVersion>10.11.0</sparkle:minimumSystemVersion>
       <description><![CDATA[
-
-<!-- RENAME_INFORMATION -->
-
-<h2>About v$version Update</h2>
+<style>
 EOF
 
->>"$targetdir/noejectdelay-appcast-devel.xml.tmp" ruby scripts/get-ul.rb < "$targetdir/noejectdelay-appcast-devel.xml"
+>>"$targetdir/noejectdelay-appcast-devel.xml.tmp" cat ../webroot/css/sparkle.css
+
+>>"$targetdir/noejectdelay-appcast-devel.xml.tmp" cat <<EOF
+</style>
+
+<h2>About v$version Update</h2>
+
+<!-- update-description-begin -->
+
+EOF
+
+>>"$targetdir/noejectdelay-appcast-devel.xml.tmp" ruby -e 'print $1.strip if /<!-- update-description-begin -->(.+?)<!-- update-description-end -->/m =~ $stdin.read' < "$targetdir/noejectdelay-appcast-devel.xml"
 
 >>"$targetdir/noejectdelay-appcast-devel.xml.tmp" cat <<EOF
 
-<a href="https://pqrs.org/osx/karabiner/noejectdelay.html#history">More</a>
+
+<!-- update-description-end -->
+<p>
+  <a href="https://pqrs.org/osx/karabiner/noejectdelay.html#history">More</a>
+</p>
 ]]>
       </description>
       <pubDate>$pubDate</pubDate>
