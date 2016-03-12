0cc3881ba3e1073a64e9f6dd8781186d12a6d4f7
Takayama Fumihiko <tekezo@pqrs.org>
2016-03-12 15:43:58 +0900

    indent

--- source/page/osx/karabiner/faq.rb
+++ source/page/osx/karabiner/faq.rb
@@ -20,62 +20,62 @@ class Faq < KarabinerBase
 
   def tabs_definition
     [
-     {
-       :href => '#general',
-       :name_l10n => {
-         :en => 'general',
-         :ja => '全般',
-       }
-     },
-     {
-       :href => '#trackpad',
-       :name_l10n => {
-         :en => 'trackpad',
-         :ja => 'trackpad',
-       }
-     },
-     {
-       :href => '#capslock',
-       :name_l10n => {
-         :en => 'caps lock key',
-         :ja => 'caps lockキー',
-       }
-     },
-     {
-       :href => '#fn',
-       :name_l10n => {
-         :en => 'fn key',
-         :ja => 'fnキー',
-       }
-     },
-     {
-       :href => '#eject',
-       :name_l10n => {
-         :en => 'eject key',
-         :ja => 'ejectキー',
-       }
-     },
-     {
-       :href => '#power',
-       :name_l10n => {
-         :en => 'power key',
-         :ja => '電源ボタン',
-       }
-     },
-     {
-       :href => '#shift',
-       :name_l10n => {
-         :en => 'shift key',
-         :ja => 'シフトキー',
-       }
-     },
-     {
-       :href => '#unicode-hex-input',
-       :name_l10n => {
-         :en => 'unicode hex input',
-         :ja => 'unicode hex input',
-       }
-     },
+      {
+        :href => '#general',
+        :name_l10n => {
+          :en => 'general',
+          :ja => '全般',
+        }
+      },
+      {
+        :href => '#trackpad',
+        :name_l10n => {
+          :en => 'trackpad',
+          :ja => 'trackpad',
+        }
+      },
+      {
+        :href => '#capslock',
+        :name_l10n => {
+          :en => 'caps lock key',
+          :ja => 'caps lockキー',
+        }
+      },
+      {
+        :href => '#fn',
+        :name_l10n => {
+          :en => 'fn key',
+          :ja => 'fnキー',
+        }
+      },
+      {
+        :href => '#eject',
+        :name_l10n => {
+          :en => 'eject key',
+          :ja => 'ejectキー',
+        }
+      },
+      {
+        :href => '#power',
+        :name_l10n => {
+          :en => 'power key',
+          :ja => '電源ボタン',
+        }
+      },
+      {
+        :href => '#shift',
+        :name_l10n => {
+          :en => 'shift key',
+          :ja => 'シフトキー',
+        }
+      },
+      {
+        :href => '#unicode-hex-input',
+        :name_l10n => {
+          :en => 'unicode hex input',
+          :ja => 'unicode hex input',
+        }
+      },
     ]
   end
 end
