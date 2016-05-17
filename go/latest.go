ba3073d8b44a48bef46a408a7acf571d0b1a36b0
Takayama Fumihiko <tekezo@pqrs.org>
2016-05-17 16:44:03 +0900

    add go

---
+++ go/latest.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/http/fcgi"
+	"regexp"
+)
+
+func redirectToLatestFile(w http.ResponseWriter, r *http.Request, filepath string) {
+	appcast, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return
+	}
+
+	re := regexp.MustCompile(`<enclosure url="(https://pqrs.org/.+?)"`)
+	res := re.FindStringSubmatch(string(appcast))
+	if len(res) <= 1 {
+		return
+	}
+
+	http.Redirect(w, r, res[1], 303)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path[1:] {
+	case "karabiner":
+		redirectToLatestFile(w, r, "../webroot/osx/karabiner/files/appcast.xml")
+	case "seil":
+		redirectToLatestFile(w, r, "../webroot/osx/karabiner/files/seil-appcast.xml")
+	case "noejectdelay":
+		redirectToLatestFile(w, r, "../webroot/osx/karabiner/files/noejectdelay-appcast.xml")
+	case "showyedge":
+		redirectToLatestFile(w, r, "../webroot/osx/ShowyEdge/files/appcast.xml")
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":9000", nil)
+
+	l, err := net.Listen("tcp", "127.0.0.1:9000")
+	if err != nil {
+		return
+	}
+	http.HandleFunc("/", handler)
+	fcgi.Serve(l, nil)
+}
