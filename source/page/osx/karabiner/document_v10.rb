2ee83e62966e395330e2a0038927a90b25346b81
Takayama Fumihiko <tekezo@pqrs.org>
2017-09-16 21:01:12 +0900

    add document_v10

---
+++ source/page/osx/karabiner/document_v10.rb
@@ -0,0 +1,148 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+
+require '../../../classes/bootstrap.rb'
+require './karabiner_base.rb'
+
+class DocumentV10 < KarabinerBase
+  def body_classes
+    [
+      {:class => 'with-page-top-fixed-alert'},
+    ]
+  end
+
+  def current_nav_path
+    '/osx/karabiner/document-v10.html'
+  end
+
+  def enable_prettify
+    true
+  end
+
+  def title
+    case @language
+    when :en
+      'Manual - ' + super()
+    when :ja
+      'マニュアル - ' + super()
+    end
+  end
+
+  def tabs_definition
+    [
+     {
+       :href => '#usage',
+       :name_l10n => {
+         :en => 'Usage',
+         :ja => '使い方',
+       }
+     },
+     {
+       :href => '#keyrepeat',
+       :name_l10n => {
+         :en => 'Change key repeat rate',
+         :ja => 'キーリピート速度の変更',
+       }
+     },
+     {
+       :href => '#quit',
+       :name_l10n => {
+         :en => 'How to quit',
+         :ja => '終了する方法',
+       }
+     },
+     {
+       :href => '#uninstall',
+       :name_l10n => {
+         :en => 'Uninstall',
+         :ja => 'アンインストール方法',
+       }
+     },
+     {
+       :href => '#check-for-updates',
+       :name_l10n => {
+         :en => 'Check for updates',
+         :ja => '最新版への更新',
+       }
+     },
+     {
+       :href => '#axnotifier',
+       :name_l10n => {
+         :en => 'AXNotifier',
+         :ja => 'AXNotifier',
+       }
+     },
+     {
+       :href => '#eventviewer',
+       :name_l10n => {
+         :en => 'Eventviewer',
+         :ja => 'イベントビューア',
+       }
+     },
+     {
+       :href => '#privatexml',
+       :name_l10n => {
+         :en => 'How to add your own settings',
+         :ja => '設定の追加',
+       }
+     },
+     {
+       :href => '#implicit',
+       :name_l10n => {
+         :en => 'Implicit behavior',
+         :ja => '暗黙の挙動',
+       }
+     },
+     {
+       :href => '#event-modification-chaining',
+       :name_l10n => {
+         :en => 'Input event modification chaining',
+         :ja => '入力イベントの変更の順番',
+       }
+     },
+     {
+       :name_l10n => {
+         :en => 'Misc.',
+         :ja => 'その他',
+       }
+     },
+     {
+       :href => '#multitouchextension',
+       :name_l10n => {
+         :en => 'Multi-touch extension',
+         :ja => 'マルチタッチ拡張',
+       }
+     },
+     {
+       :href => '#profiles',
+       :name_l10n => {
+         :en => 'Multiple profiles',
+         :ja => '複数の設定の使い分け',
+       }
+     },
+     {
+       :href => '#commandlineinterface',
+       :name_l10n => {
+         :en => 'Command line interface',
+         :ja => 'コマンドラインインターフェース',
+       }
+     },
+     {
+       :href => '#preferences',
+       :name_l10n => {
+         :en => 'The location of the configuration file',
+         :ja => '設定ファイルの場所',
+       }
+     },
+     {
+       :href => '#debugmode',
+       :name_l10n => {
+         :en => 'debug mode',
+         :ja => 'デバッグモード',
+       }
+     },
+    ]
+  end
+end
+
+PqrsUtil.make_page('/osx/karabiner/document_v10.html', DocumentV10)
---
+++ webroot/cache/pqrs.js.3bf75c0e6a2fadabcc4147fa725d9740e841f9c6.f3bb6d130ac6f99377f37928956baa5d6966e806.js
@@ -0,0 +1,43 @@
+! function($) {
+    'use strict';
+
+    $(function() {
+        /* ------------------------------------------------------------ */
+        /* from bs/assets/js/application.js */
+        var $window = $(window);
+        var $body = $(document.body);
+        var $sideBar = $('#bs-docs-sidebar');
+
+        var navHeight = $('.navbar').outerHeight(true) + 10;
+
+        $body.scrollspy({
+            target: '.bs-docs-sidebar'
+        });
+        $window.on('load', function() {
+            $body.scrollspy('refresh');
+        });
+
+        // set bs-docs-sidebar height
+        if ($sideBar.length > 0 && $sideBar.offset().left < 400) {
+            // $sideBar block is wrapped because the window.width is too small.
+            // We should not set $sideBar.height.
+            $sideBar.hide();
+        } else {
+            $sideBar.height($window.height() - 250);
+            $sideBar.css('overflow', 'auto');
+        }
+
+        /* ------------------------------------------------------------ */
+        /* pqrs.org */
+        $('h1[id], h2[id], h3[id]').each(function() {
+            var $this = $(this);
+            $this.prepend(' <a class="js-id-link" href="#' + $this.attr('id') + '"><small><span class="glyphicon glyphicon-link"></span></small></a> ');
+            $this.wrapInner('<div class="js-id-link-wrapper"></div>');
+        });
+
+        $('.modal-dialog').click(function() {
+            $(this).parent().modal('hide');
+        });
+    });
+
+}(window.jQuery);
