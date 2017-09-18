805bb933b80fcc8543a318e2b81cf65d27af2ca0
Takayama Fumihiko <tekezo@pqrs.org>
2017-09-18 22:10:38 +0900

    remove Japanese resources

--- source/page/osx/karabiner/xml.rb
+++ source/page/osx/karabiner/xml.rb
@@ -26,200 +26,114 @@ class Xml < KarabinerBase
   def tabs_definition
     [
       {
-        :name_l10n => {
-          :en => 'examples',
-          :ja => 'examples',
-        }
+        :name => 'examples',
       },
       {
         :href => '#examples',
-        :name_l10n => {
-          :en => 'examples',
-          :ja => 'examples',
-        },
+        :name => 'examples',
       },
       {
         :href => '#autogen-examples',
-        :name_l10n => {
-          :en => 'existing settings',
-          :ja => 'existing settings',
-        },
+        :name => 'existing settings',
       },
       {
-        :name_l10n => {
-          :en => 'basic',
-          :ja => 'basic',
-        }
+        :name => 'basic',
       },
       {
         :href => '#basic-syntax',
-        :name_l10n => {
-          :en => 'basic syntax',
-          :ja => 'basic syntax',
-        }
+        :name => 'basic syntax',
       },
       {
-        :name_l10n => {
-          :en => 'filters',
-          :ja => 'filters',
-        }
+        :name => 'filters',
       },
       {
         :href => '#filter-overview',
-        :name_l10n => {
-          :en => 'overview',
-          :ja => 'overview',
-        }
+        :name => 'overview',
       },
       {
         :href => '#appdef',
-        :name_l10n => {
-          :en => 'specify application',
-          :ja => 'specify application',
-        }
+        :name => 'specify application',
       },
       {
         :href => '#bundleidentifieroverridedef',
-        :name_l10n => {
-          :en => 'override the bundle identifier',
-          :ja => 'override the bundle identifier',
-        }
+        :name => 'override the bundle identifier',
       },
       {
         :href => '#windownamedef',
-        :name_l10n => {
-          :en => 'specify window name (window title)',
-          :ja => 'specify window name (window title)',
-        }
+        :name => 'specify window name (window title)',
       },
       {
         :href => '#uielementroledef',
-        :name_l10n => {
-          :en => 'specify ui element role',
-          :ja => 'specify ui element role',
-        }
+        :name => 'specify ui element role',
       },
       {
         :href => '#devicedef',
-        :name_l10n => {
-          :en => 'specify device',
-          :ja => 'specify device',
-        }
+        :name => 'specify device',
       },
       {
         :href => '#inputsourcedef',
-        :name_l10n => {
-          :en => 'specify input source',
-          :ja => 'specify input source',
-        }
+        :name => 'specify input source',
       },
       {
-        :name_l10n => {
-          :en => 'special key code',
-          :ja => 'special key code',
-        }
+        :name => 'special key code',
       },
       {
         :href => '#vkopenurldef',
-        :name_l10n => {
-          :en => 'launch app / open url / execute shell commands',
-          :ja => 'launch app / open url / execute shell commands',
-        }
+        :name => 'launch app / open url / execute shell commands',
       },
       {
         :href => '#vkchangeinputsourcedef',
-        :name_l10n => {
-          :en => 'change input source',
-          :ja => 'change input source',
-        }
+        :name => 'change input source',
       },
       {
-        :name_l10n => {
-          :en => 'advanced features',
-          :ja => 'advanced features',
-        }
+        :name => 'advanced features',
       },
       {
         :href => '#include',
-        :name_l10n => {
-          :en => 'include external XML',
-          :ja => 'include external XML',
-        }
+        :name => 'include external XML',
       },
       {
         :href => '#replacementdef',
-        :name_l10n => {
-          :en => 'string replacement',
-          :ja => 'string replacement',
-        }
+        :name => 'string replacement',
       },
       {
         :href => '#keycode',
-        :name_l10n => {
-          :en => 'new keycode definition',
-          :ja => 'new keycode definition',
-        }
+        :name => 'new keycode definition',
       },
       {
         :href => '#modifier',
-        :name_l10n => {
-          :en => 'new virtual modifier definition',
-          :ja => 'new virtual modifier definition',
-        }
+        :name => 'new virtual modifier definition',
       },
       {
         :href => '#expand',
-        :name_l10n => {
-          :en => "expand prepared settings's capabilities",
-          :ja => "expand prepared settings's capabilities",
-        }
+        :name => "expand prepared settings's capabilities",
       },
       {
         :href => '#prepared',
-        :name_l10n => {
-          :en => 'modify prepared settings',
-          :ja => 'modify prepared settings',
-        }
+        :name => 'modify prepared settings',
+
       },
       {
-        :name_l10n => {
-          :en => '<autogen> syntax',
-          :ja => '<autogen> syntax',
-        }
+        :name => '<autogen> syntax',
       },
       {
         :href => '#autogen-syntax',
-        :name_l10n => {
-          :en => '<autogen> syntax',
-          :ja => '<autogen> syntax',
-        }
+        :name => '<autogen> syntax',
       },
       {
         :href => '#filter-syntax',
-        :name_l10n => {
-          :en => 'filters',
-          :ja => 'filters',
-        }
+        :name => 'filters',
       },
       {
-        :name_l10n => {
-          :en => 'tips',
-          :ja => 'tips',
-        }
+        :name => 'tips',
       },
       {
         :href => '#tips-unicode-hex-input',
-        :name_l10n => {
-          :en => 'unicode hex input',
-          :ja => 'unicode hex input',
-        }
+        :name => 'unicode hex input',
       },
       {
         :href => '#tips-string-input',
-        :name_l10n => {
-          :en => 'string input',
-          :ja => 'string input',
-        }
+        :name => 'string input',
       },
     ]
   end
