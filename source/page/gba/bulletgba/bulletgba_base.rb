c51f6959e84ba4fb9d4f068568d075ef35daca92
Takayama Fumihiko <tekezo@pqrs.org>
2017-09-18 18:10:25 +0900

    remove Japanese resources

--- source/page/gba/bulletgba/bulletgba_base.rb
+++ source/page/gba/bulletgba/bulletgba_base.rb
@@ -3,44 +3,27 @@
 
 class BulletgbaBase < PqrsBase
   def title
-    case @language
-    when :en
-      'BulletGBA - Software for Gameboy Advance'
-    when :ja
-      'BulletGBA - ゲームボーイアドバンス用のソフトウェア'
-    end
+    'BulletGBA - Software for Gameboy Advance'
   end
 
   def navs_left_data
     [
-     {
-       :href => '/gba/bulletgba/index.html',
-       :name_l10n => {
-         :en => 'BulletGBA',
-         :ja => 'BulletGBA',
-       },
-     },
-     {
-       :href => '/gba/bulletgba/manual.html',
-       :name_l10n => {
-         :en => 'Manual',
-         :ja => 'マニュアル',
-       },
-     },
-     {
-       :href => '/gba/bulletgba/history.html',
-       :name_l10n => {
-         :en => 'Version History',
-         :ja => '更新履歴',
-       },
-     },
-     {
-       :href => '/gba/bulletgba/source.html',
-       :name_l10n => {
-         :en => 'Development',
-         :ja => '開発',
-       },
-     },
+      {
+        :href => '/gba/bulletgba/index.html',
+        :name => 'BulletGBA',
+      },
+      {
+        :href => '/gba/bulletgba/manual.html',
+        :name => 'Manual',
+      },
+      {
+        :href => '/gba/bulletgba/history.html',
+        :name => 'Version History',
+      },
+      {
+        :href => '/gba/bulletgba/source.html',
+        :name => 'Development',
+      },
     ]
   end
 end
--- source/page/gba/bulletgba/history.rb
+++ source/page/gba/bulletgba/history.rb
@@ -7,13 +7,12 @@ require './bulletgba_base.rb'
 class History < BulletgbaBase
   def histories
     data = [
-            {
-              :version => '5.0',
-              :updated => '2007-05-21',
-              :link => 'files/bulletgba-5.0.rom.gba.zip',
-              :source => 'files/bulletgba-5.0.tar.gz',
-              :body_l10n => {
-                :en => <<EOS,
+      {
+        :version => '5.0',
+        :updated => '2007-05-21',
+        :link => 'files/bulletgba-5.0.rom.gba.zip',
+        :source => 'files/bulletgba-5.0.tar.gz',
+        :body => <<EOS,
 <ul>
 <li>Added sound composed by <a href="http://www.nullsleep.com/" target="_blank">Nullsleep</a>.</li>
 <li>Improved graphic resources.</li>
@@ -22,24 +21,13 @@ class History < BulletgbaBase
 <li>Added some BulletML.</li>
 </ul>
 EOS
-                :ja => <<EOS,
-<ul>
-<li><a href="http://www.nullsleep.com/" target="_blank">Nullsleep</a>氏によるサウンドを追加しました。
-<li>ドット絵を改良しました。</li>
-<li>シューティングモードのステージを改良しました。</li>
-<li>BulletMLの取り扱いを改良しました。</li>
-<li>いくつかのBulletMLを追加しました。</li>
-</ul>
-EOS
-              },
-            },
-            {
-              :version => '4.0',
-              :updated => '2006-05-29',
-              :link => 'files/bulletgba-4.0.rom.gba.zip',
-              :source => 'files/bulletgba-4.0.tar.gz',
-              :body_l10n => {
-                :en => <<EOS,
+      },
+      {
+        :version => '4.0',
+        :updated => '2006-05-29',
+        :link => 'files/bulletgba-4.0.rom.gba.zip',
+        :source => 'files/bulletgba-4.0.tar.gz',
+        :body => <<EOS,
 <ul>
 <li>Added "Replay" feature. You can record your game play into SRAM. ("Challenge &amp; Shooting Mode")</li>
 <li>Added demo replay data.</li>
@@ -47,45 +35,26 @@ EOS
 <li>Improved font graphic.</li>
 </ul>
 EOS
-                :ja => <<EOS,
-<ul>
-<li>リプレイ機能を追加しました。 チャレンジモード、シューティングモードのリプレイをSRAMに保存できます。</li>
-<li>デモのリプレイデータを追加しました。</li>
-<li>Shooting Level 4を追加しました。</li>
-<li>フォントを改良しました。</li>
-</ul>
-EOS
-              },
-            },
-            {
-              :version => '3.0',
-              :updated => '2006-05-02',
-              :link => 'files/bulletgba-3.0.rom.gba.zip',
-              :source => 'files/bulletgba-3.0.tar.gz',
-              :body_l10n => {
-                :en => <<EOS,
+      },
+      {
+        :version => '3.0',
+        :updated => '2006-05-02',
+        :link => 'files/bulletgba-3.0.rom.gba.zip',
+        :source => 'files/bulletgba-3.0.tar.gz',
+        :body => <<EOS,
 <ul>
 <li>Added Shooting Mode.</li>
 <li>Improved graphic resources.</li>
 <li>Improved BulletML handling.</li>
 </ul>
 EOS
-                :ja => <<EOS,
-<ul>
-<li>シューティングモードを追加しました。</li>
-<li>ドット絵を改良しました。</li>
-<li>BulletMLの取り扱いを改良しました。</li>
-</ul>
-EOS
-              },
-            },
-            {
-              :version => '2.0',
-              :updated => '2006-04-08',
-              :link => 'files/bulletgba-2.0.rom.gba.zip',
-              :source => 'files/bulletgba-2.0.tar.gz',
-              :body_l10n => {
-                :en => <<EOS,
+      },
+      {
+        :version => '2.0',
+        :updated => '2006-04-08',
+        :link => 'files/bulletgba-2.0.rom.gba.zip',
+        :source => 'files/bulletgba-2.0.tar.gz',
+        :body => <<EOS,
 <ul>
 <li>Improved sprite handling. (less flicker, less vanishing of sprite.)</li>
 <li>Added slow mode. (a.k.a. kakusei in Espgaluda.) You can toggle slow mode by press START or SELECT.</li>
@@ -93,58 +62,32 @@ EOS
 <li>Added BulletsMorph.</li>
 </ul>
 EOS
-                :ja => <<EOS,
-<ul>
-<li>描写エンジンを改良しました。 ちらつきやスプライト欠けが軽減されています。</li>
-<li>弾速が遅くなるモード（エスプガルーダの覚聖モード）を追加しました。STARTもしくはSELECTボタンで切り替えます。</li>
-<li>チャレンジモードを追加しました。</li>
-<li>BulletsMorphモードを追加しました。</li>
-</ul>
-EOS
-              },
-            },
-            {
-              :version => '1.1',
-              :updated => '2006-03-09',
-              :link => 'files/bulletgba-1.1.gba.zip',
-              :source => 'files/bulletgba-1.1.tar.gz',
-              :body_l10n => {
-                :en => <<EOS,
+      },
+      {
+        :version => '1.1',
+        :updated => '2006-03-09',
+        :link => 'files/bulletgba-1.1.gba.zip',
+        :source => 'files/bulletgba-1.1.tar.gz',
+        :body => <<EOS,
 <ul>
 <li>Added some BulletML.</li>
 <li>Optimized game engine.</li>
 <li>Some minor improvements.</li>
 </ul>
 EOS
-                :ja => <<EOS,
-<ul>
-<li>ゲームエンジンの最適化を行いました。</li>
-<li>いくつかのBulletMLを追加しました。</li>
-<li>その他、細かい修正を行いました。</li>
-</ul>
-EOS
-              },
-            },
-            {
-              :version => '1.0',
-              :updated => '2006-02-27',
-              :link => 'files/bulletgba-1.0.gba.zip',
-              :source => 'files/bulletgba-1.0.tar.gz',
-              :body_l10n => {
-                :en => <<EOS,
+      },
+      {
+        :version => '1.0',
+        :updated => '2006-02-27',
+        :link => 'files/bulletgba-1.0.gba.zip',
+        :source => 'files/bulletgba-1.0.tar.gz',
+        :body => <<EOS,
 <ul>
 <li>Initial release.</li>
 </ul>
 EOS
-                :ja => <<EOS,
-<ul>
-<li>最初のリリース。</li>
-</ul>
-EOS
-              },
-            },
-           ]
-    set_l10n_text(data)
+      },
+    ]
   end
 end
 
