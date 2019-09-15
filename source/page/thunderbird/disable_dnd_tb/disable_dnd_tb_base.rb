d70bc8dbe1e19944c70a204a52676a2d7779c972
Takayama Fumihiko <tekezo@pqrs.org>
2019-09-15 14:26:36 +0900

    add og_*

--- source/page/thunderbird/disable_dnd_tb/disable_dnd_tb_base.rb
+++ source/page/thunderbird/disable_dnd_tb/disable_dnd_tb_base.rb
@@ -6,6 +6,18 @@ class DisableDndTbBase < PqrsBase
     'Disable DragAndDrop - Thunderbird Extension'
   end
 
+  def og_title
+    'Disable DragAndDrop'
+  end
+
+  def og_description
+    'Thunderbird add-on which disables drag and drop on the folder tree in order to prevent unintended folder movement.'
+  end
+
+  def og_image
+    'https://pqrs.org/thunderbird/extensions/disable_dnd_tb/img/logo.png'
+  end
+
   def navs_left_data
     [
       {
